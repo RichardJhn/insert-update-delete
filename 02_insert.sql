dfabde960b7408876a062392074871355f80b74f
RichardJhn <rjohn@my.yearupunited.org>
2025-11-20 16:04:23 -0500

    02

---
+++ 02_insert.sql
@@ -0,0 +1,4 @@
+use northwind;
+
+insert into products (ProductID, ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel)
+values ('78', 'Sasquatch Ale', '30', '1', 'bottles', '14.0000', '111', '0', '15')
